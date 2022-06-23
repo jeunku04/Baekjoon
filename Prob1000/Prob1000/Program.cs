6cb24402d30eb825ace47f1dd51340f381e4693f
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-06-23 15:25:09 +0900

    Add files via upload

---
+++ Prob1000/Prob1000/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prob1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrinput = input.Split();
+            int a = int.Parse(arrinput[0]);
+            int b = int.Parse(arrinput[1]);
+            Console.WriteLine(a + b);
+            
+        }
+    }
+}
