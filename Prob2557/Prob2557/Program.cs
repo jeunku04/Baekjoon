5759367b5b94e29e990adba7ecbf7e744cb650b8
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-06-27 09:05:57 +0900

    Add files via upload

---
+++ Prob2557/Prob2557/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prob2557
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
