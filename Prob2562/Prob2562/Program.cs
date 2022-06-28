06c55638529f646363da652a72b774df9ebee9e5
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-06-28 09:06:27 +0900

    Add files via upload

---
+++ Prob2562/Prob2562/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prob2562
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // 최대값 (2562)
+            int n = 9;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                a[i] = int.Parse(input);
+            }
+
+            int max = int.MinValue;
+            int maxi = 0;
+            for(int i=0; i<n; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+                    maxi = i;
+                Console.WriteLine(max);
+                Console.WriteLine(maxi);
+            }
+
+            
+        }
+    }
+}
