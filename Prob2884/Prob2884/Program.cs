e9809d2b1b1b7c5a59c96f7dda75fccbde63a753
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-07-01 15:46:11 +0900

    Add files via upload

---
+++ Prob2884/Prob2884/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prob2884
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrInput = input.Split();
+
+            int h = int.Parse(arrInput[0]);
+            int m = int.Parse(arrInput[1]);
+
+            m -= 45;
+            if(m<0)
+            {
+                m += 60;
+                --h;
+                if (h < 0)
+                    h += 24;
+            }
+                Console.WriteLine("{0} {1}", h, m);
+
+
+
+        }
+    }
+}
---
+++ Prob2947/Prob2947/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Prob2947
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // 나무 조각 (2947)
+            string input = Console.ReadLine();
+            string[] arrInput = input.Split();
+
+            int n = 5;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = int.Parse(arrInput[i]);
+
+            if(a[0] > a[1])
+            {
+                int b = a[0];
+                a[0] = a[1];
+                a[1] = b;
+            }
+            else if (a[1] > a[2])
+            {
+                int b = a[1];
+                a[1] = a[2];
+                a[2] = b;
+            }
+            else if (a[2] > a[3])
+            {
+                int b  = a[2];
+                a[2] = a[3];
+                a[4] = b;
+            }
+            else if (a[3] > a[4])
+            {
+                int b  = a[3];
+                a[3] = a[4];
+                a[4] = b;
+            }
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", a[0], a[1], a[2], a[3], a[4]);
+        }
+    }
+}
