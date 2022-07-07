cb1bc36ccd0e2e2d34ce0b4071fd59789e1983c2
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-07-07 16:29:14 +0900

    Add files via upload

---
+++ Prob10178/Prob10178/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prob10178
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // 할로윈의 사탕 (10178)
+            string input = Console.ReadLine();
+            int tc = int.Parse(input);
+            for (int t = 0; t < tc; t++)
+            {
+                input = Console.ReadLine();
+                string[] arrInput = input.Split();
+                int c = int.Parse(arrInput[0]);
+                int v = int.Parse(arrInput[1]);
+                Console.WriteLine("You get {0} piece(s) and your dad gets {1} piece(s).", c / v, c % v);
+            }
+        }
+    }
+}
---
+++ Prob10430/Prob10430/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prob1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrinput = input.Split();
+            int A = int.Parse(arrinput[0]);
+            int B = int.Parse(arrinput[1]);
+            int C = int.Parse(arrinput[2]);
+            Console.WriteLine((A + B) % C);
+            Console.WriteLine(((A % C) + (B % C)) % C);
+            Console.WriteLine((A * B) % C);
+            Console.WriteLine((A % C) * (B % C) % C);
+
+        }
+    }
+}
