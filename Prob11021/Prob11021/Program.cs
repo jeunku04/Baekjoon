d935b75ec2bf84ff29b1c0cd12624c91775acc8e
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-07-19 12:32:03 +0900

    Add files via upload

---
+++ Prob11021/Prob11021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prob11021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+            int aWinCnt = 0;
+            int bWinCnt = 0;
+            for ()
+            {
+                input = Console.ReadLine();
+                string[] arrInput = input.Split();
+                int a = int.Parse(arrInput[0]);
+                int b = int.Parse(arrInput[1]);
+
+                
+            }
+            Console.WriteLine("{0} {1}", aWinCnt, bWinCnt);
+        }
+    }
+}
---
+++ Prob14681/Prob14681/Program.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+
+    namespace Prob14681
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {   // 사분면 고르기 (14681)
+                string input = Console.ReadLine();
+                int x = int.Parse(input);
+                input = Console.ReadLine();
+                int y = int.Parse(input);
+
+                if (x > 0)
+                {
+                    if (y > 0)
+                        Console.WriteLine(1);
+                    else
+                        Console.WriteLine(4);
+                }
+                else
+                if(x<0)
+                {
+                    if (y > 0)
+                        Console.WriteLine(2);
+                    else
+                        Console.WriteLine(3);
+                }
+            }
+        }
+    }
