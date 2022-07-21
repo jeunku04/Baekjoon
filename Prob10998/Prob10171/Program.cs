84c790d488deccb1c2017e3198e8497d5a1fc5b9
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-07-21 16:28:49 +0900

    Add files via upload

---
+++ Prob10998/Prob10171/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prob10998
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrInput = input.Split();
+            int a = int.Parse(arrInput[0]);
+            int b = int.Parse(arrInput[1]);
+            Console.WriteLine(a * b);
+        }
+    }
+}
