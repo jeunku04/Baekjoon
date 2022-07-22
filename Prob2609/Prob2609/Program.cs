396dd61e2edc6cd4ab9f5882ec48f0510a9463bf
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-07-22 16:10:41 +0900

    Add files via upload

---
+++ Prob2609/Prob2609/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prob2609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrInput = input.Split();
+            int a = int.Parse(arrInput[0]);
+            int b = int.Parse(arrInput[1]);
+            int i;
+            int num1 = 0, num2 = 0;
+            for (i = 1;  i <= a && i <= b; i++)
+                if (a % i + b % i == 0)
+                {
+                    num1 = i;
+                }
+            for (i = a * b; i >= 1; i--)
+                if (i % a + i % b == 0)
+                {
+                    num2 = i;
+                }
+            Console.WriteLine("{0} {1}",num1, num2);
+        }
+    }
+}
