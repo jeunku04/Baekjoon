1a257bc1acdf5f9ffc0ccb0d254885b8fd509ee0
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-08-19 11:35:52 +0900

    Add files via upload

---
+++ Prob10926/Prob10926/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prob10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine(name+"??!");
+        }
+    }
+}
