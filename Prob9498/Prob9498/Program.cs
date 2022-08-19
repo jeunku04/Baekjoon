201faa890aa3d91eb0af9f0222d38d3c11c833d0
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-08-19 12:01:03 +0900

    Add files via upload

--- Prob9498/Prob9498/Program.cs
+++ Prob9498/Prob9498/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Prob9498
+namespace Prob1000
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            string input = Console.ReadLine();  
+            string[] arrInput = input.Split();
+            int a = int.Parse(arrInput[0]);
+            if(a >= 90)
+                Console.WriteLine("A");
+            else if (a >= 80)
+                Console.WriteLine("B");
+            else if (a >= 70)
+                Console.WriteLine("C");
+            else if (a >= 60)
+                Console.WriteLine("D");
+            else
+                Console.WriteLine("F");
         }
     }
 }
