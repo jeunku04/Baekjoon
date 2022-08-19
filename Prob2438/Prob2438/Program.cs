af7d1c6ab9f1fe6db6d029557cd299122e039862
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-08-19 12:07:19 +0900

    Add files via upload

---
+++ Prob2438/Prob2438/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Prob2438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // 별 찍기 - 1 (2438)
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
