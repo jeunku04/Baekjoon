fac5b4799afccb319cfda7aee27928e506544c7c
jeunku04 <108050242+jeunku04@users.noreply.github.com>
2022-08-25 09:24:53 +0900

    Add files via upload

---
+++ Prob18108/Prob18108/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prob18108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
+            Console.WriteLine(n-543);
+        }
+    }
+}
